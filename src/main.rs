025b6e0e9d687117a707adbc366115080f080139
engimatic <eternity_bliss@sina.cn>
2023-10-16 23:31:46 +0800

    feat(0.1):search

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,27 @@
-use std::{env, process};
-use rust_study::{Config, run};
+use std::io::{Read, Write};
+use std::fs;
+use std::net::TcpListener;
+use std::net::TcpStream;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let config = Config::build(&args).unwrap_or_else(|err| {
-        eprintln!("Problem parsing arguments: {err}");
-        process::exit(1);
-    });
-
-    if let Err(e) = run(config) {
-        eprintln!("Application error: {e}");
-        process::exit(1);
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer ).unwrap();
+    let get = b"GET / HTTP/1.1\r\n";
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+    };
+    let content = fs::read_to_string(filename).unwrap();
+    let response = format!("{status_line}{content}");
+    stream.write_all(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
